eebef7bb864b6f4440ba1e529bd26b55687dbd13
shubhamsingh <singhshubhamkumar66@gmail.com>
2026-01-07 02:57:12 +0530

    Main: add cross1

--- src/main/java/com/portfolio/app/config/CorsConfig.java
+++ src/main/java/com/portfolio/app/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "https://myprotfolio-frontend-1xh9l48yh-shubhams-projects-31c38d4e.vercel.app"
+                "https://myprotfolio-frontend-c4ypzcwdt-shubhams-projects-31c38d4e.vercel.app"
+
         ));
 
 
